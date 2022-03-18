f11e03e09d711f03da2a8caa00717667517b1c35
gyowoo1113 <yereube@naver.com>
2022-03-18 11:36:15 +0900

    Update 대신 Start에서 invoke를 사용해서 3초 주기로 print

--- TU_STUDY2/Assets/Scenes/study.cs
+++ TU_STUDY2/Assets/Scenes/study.cs
@@ -7,18 +7,13 @@ public class study : MonoBehaviour
 {
     void Start()
     {
-        print("게임이 시작되었습니다.");
+        Invoke("PrintText",3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _atttackIntervalTimer += Time.deltaTime;
-        if (_atttackIntervalTimer > 3f)
-        {
-            PrintText();
-            _atttackIntervalTimer = 0f;
-        }
+
     }
 
     void PrintText()
