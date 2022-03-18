b0b3cf54067e8fb18f27b96bbc16073dca846dba
gyowoo1113 <yereube@naver.com>
2022-03-18 11:32:34 +0900

    Serialization test
    using System 필요

--- TU_STUDY2/Assets/Scenes/study.cs
+++ TU_STUDY2/Assets/Scenes/study.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[Serializable]
+public class ComplexType
+{
+    public float name;
+    public int age;
+    public float height;
+}
 
 public class study : MonoBehaviour
 {
-    void Start
-    (
+    public ComplexType studyName;
+    
+    void Start()
+    {
 
-    )
+    }
 
-    void Update() {
+    void Update()
+    {
         
     }
 }
