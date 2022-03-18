66aba0d8897218c02517d92dbe9979a1afc61778
gyowoo1113 <yereube@naver.com>
2022-03-18 11:35:15 +0900

    Update에서 atttackIntervalTimer Test

--- TU_STUDY2/Assets/Scenes/study.cs
+++ TU_STUDY2/Assets/Scenes/study.cs
@@ -10,36 +10,19 @@ public class study : MonoBehaviour
         print("게임이 시작되었습니다.");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        print("컴포넌트가 업데이트 되었습니다.");
-    }
-    private void OnEnable()
-    {
-        print("컴포넌트가 활성화 되었습니다.");
-    }
-
-    private void OnDisable()
-    {
-        print("컴포넌트가 비활성화 되었습니다.");
+        _atttackIntervalTimer += Time.deltaTime;
+        if (_atttackIntervalTimer > 3f)
+        {
+            PrintText();
+            _atttackIntervalTimer = 0f;
+        }
     }
 
-    // 언리얼 - 업데이트를 tick으로, 유니티는 별도의 업데이트가 들어감
-    // 똑같은 인터벌로 계속 실행되게끔
-    // 물리적인 연산, 같은주기로 업데이트 해야하는것들 
-    // Update보다 이전에 호출됨
-    private void FixedUpdate()
+    void PrintText()
     {
-        print("컴포넌트가 주기적으로 업데이트 됩니다.");
-    }
-
-    // UI 표현, 카메라 이동
-    // Update 에서 실행 혹은 사용자 input에 대한 결과값 표시
-    // Update 안에서 이걸 하게되면 카메라가 지글지글 거리는 등
-    // 움직임이 자연스럽지 못하게 될 수 있음
-    // Update 바로 뒤에 실행됨
-    private void LateUpdate()
-    {
-        print("업데이트 뒤에 실행됩니다.");
+        print("공격!!!");
     }
 }
