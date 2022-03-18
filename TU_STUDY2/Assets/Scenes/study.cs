5c0b5b563b1832250685f1362d5f0ab8bd8a0c97
gyowoo1113 <yereube@naver.com>
2022-03-18 11:30:56 +0900

    실습을 위한 study.cs 추가

---
+++ TU_STUDY2/Assets/Scenes/study.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class study : MonoBehaviour
+{
+    void Start
+    (
+
+    )
+
+    void Update() {
+        
+    }
+}
