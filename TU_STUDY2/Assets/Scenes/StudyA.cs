65ef04a7c9432815eb5baa83280846d52d257d95
gyowoo1113 <yereube@naver.com>
2022-03-18 11:54:51 +0900

    FindObjectOfType으로 컴포넌트 검색

--- TU_STUDY2/Assets/Scenes/StudyA.cs
+++ TU_STUDY2/Assets/Scenes/StudyA.cs
@@ -8,7 +8,11 @@ public class StudyA : MonoBehaviour
 
     private void Start()
     {
-        studyBGameObject = GameObject.Find("B");
+        // 컴포넌트 찾은 뒤 gameObject 연결
+        // 단 한번만 사용되는 곳에서 쓰기엔 편리함
+        // 단 Update 등에선 GameObject에서 있는 Hieracy를 모두 찾아보는 것이기 때문에
+        // 퍼포먼스 문제가 발생할 수 있음
+        studyBGameObject = GameObject.FindObjectOfType<StudyB>().gameObject;
         print(studyBGameObject.name);
     }
 }
