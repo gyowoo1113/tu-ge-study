0ea28aba68e47407a463f3ca58574c6e30d0326e
gyowoo1113 <yereube@naver.com>
2022-04-01 11:59:16 +0900

    Feat: 이동 구현

--- TU_STUDY4/Assets/NewBall.cs
+++ TU_STUDY4/Assets/NewBall.cs
@@ -5,8 +5,22 @@ using UnityEngine.InputSystem;
 
 public class NewBall : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    private Vector3 _direction;
+
+    private void Awake() 
+    {
+        _rigidbody = GetComponent<Rigidbody>();    
+    }
+
     public void OnMove(InputValue value)
     {
-        print(value.Get<Vector2>());
+        var rawValue = value.Get<Vector2>();
+        _direction = new Vector3(rawValue.x,0,rawValue.y);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(_direction);
     }
 }
