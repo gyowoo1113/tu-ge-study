4839aa637f4088804cf528c337b0d4c21c615930
gyowoo1113 <yereube@naver.com>
2022-04-01 12:03:46 +0900

    Feat: Add ActionDisplayMapText

---
+++ TU_STUDY4/Assets/ActionMapDisplayText.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ActionMapDisplayText : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    private TMP_Text _text;
+    
+    private void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        _text.text = playerInput.currentActionMap.name;
+    }
+}
