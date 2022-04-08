71de39a4e7accb36dc5699c353ce3162bf829830
gyowoo1113 <yereube@naver.com>
2022-04-08 12:00:32 +0900

    Test: Scroll View test

---
+++ TU_STUDY5/Assets/Scenes/InputFieldTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFieldTest : MonoBehaviour
+{
+    public void onValueChanegd(string value)
+    {
+        print($"text: {value}");
+    }
+}
