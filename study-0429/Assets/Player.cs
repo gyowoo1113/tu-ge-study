fbc9e5f7bf896208db9fbf72691c7892bcaf42c1
gyowoo1113 <yereube@naver.com>
2022-04-29 10:43:33 +0900

    Feat : Linq 사용해서 array 형태로 itemName 뽑기

--- study-0429/Assets/Player.cs
+++ study-0429/Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,10 +12,13 @@ public class Player : MonoBehaviour
             GameManager.Instance.coin++;
             var itemDatas = GameManager.Instance.itemDatabase.itemDatas;
 
-            foreach (var itemData in itemDatas)
-            {
-                print($"{itemData.itemName}: LV.{itemData.itemLevel}");
-            }
+            var itemLowerthan3 = 
+                    from item in itemDatas
+                    where item.itemLevel < 3
+                    select item.itemName;
+            
+            foreach(var itemName in itemLowerthan3)
+                print(itemName);
         }
     }
 }
