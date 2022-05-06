e2fb13addc7c810f6951ac040d304aa0e35051bc
gyowoo1113 <yereube@naver.com>
2022-05-06 09:51:11 +0900

    Feat : instance EventManager 작성

---
+++ practice-0506/Assets/EventManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EventManager : MonoBehaviour
+{
+    private static EventManager _instance;
+
+    private Dictionary<string, Action> _eventDb;
+    
+    private void Awake()
+    {
+        _eventDb = new Dictionary<string, Action>();
+    }
+
+    public static EventManager Instance
+    {
+        get
+        {
+            if (_instance == null) _instance = FindObjectOfType<EventManager>();
+            if (_instance == null)
+            {
+                var go = new GameObject(nameof(EventManager));
+                _instance = go.AddComponent<EventManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public void Subscribe(string eventName, Action action)
+    {
+        if (_eventDb.ContainsKey(eventName))
+        {
+            _eventDb.Add(eventName,action);
+        }
+        else
+        {
+            _eventDb[eventName] = action;
+        }
+    }
+}
