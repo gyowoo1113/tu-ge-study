549dd807273d0d9a790d631ebcb211e09146dde1
gyowoo1113 <yereube@naver.com>
2022-05-13 09:48:24 +0900

    Test :  0513 skeleton

---
+++ practice-0506/Assets/NPC.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private NpcState _npcState;
+    public Transform target;
+    public float sightAngle = 45f;
+    public float sightRange = 2f;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start() 
+    {
+        EventManager.Instance.Subscribe("game_over", OnGameOver);    
+    }
+
+    private void Update()
+    {
+        _agent.destination = target.position;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(NpcRoutine());
+    }
+
+    private void OnDisable() 
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnGameOver(object param)
+    {
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator NpcRoutine()
+    {
+        _npcState = NpcState.Finding;
+
+        while(true)
+        {
+            switch(_npcState)
+            {
+                case NpcState.Finding:
+                    yield return Find();
+                    break;
+                case NpcState.Chasing:
+                    yield return Chase();
+                    break;
+                case NpcState.Attacking:
+                    yield return Attack();
+                    break;
+            }
+            yield return null;
+        }
+    }
+    
+    IEnumerator Find()
+    {
+        var dir = Random.insideUnitSphere;
+        var dest = dir*5f + transform.position;
+        NavMesh.SamplePosition(dest,out var hit,5f,NavMesh.AllAreas);
+        _agent.destination = hit.position;
+
+        
+        while (_agent.remainingDistance > 0.1f)
+        {
+            var dirFromTarget = (target.position - transform.position).normalized;
+            var dot = Vector3.Dot(dirFromTarget,transform.forward);
+            var degree = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+            Physics.Raycast(transform.position, transform.forward, out var hitInfo);
+
+            if (hitInfo.collider != null &&
+                hitInfo.collider.gameObject.name =="Target" &&
+                degree < sightAngle &&
+                Vector3.Distance(target.position, transform.position) < sightRange)
+            {
+                _npcState = NpcState.Chasing;
+                break;
+            }
+            
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    IEnumerator Chase()
+    {
+        while (_agent.remainingDistance > 0.1f)
+        {
+            _agent.destination = target.position;
+            yield return null;
+        }
+
+        _npcState = NpcState.Attacking;
+    }
+
+    IEnumerator Attack()
+    {
+        while(_agent.remainingDistance < 0.1f)
+        {
+            _agent.destination = target.position;
+            print("Attack");
+            yield return null;
+        }
+
+        _npcState = NpcState.Finding;
+    }
+}
